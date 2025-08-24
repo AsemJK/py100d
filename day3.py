90a5dbce83c154b77334626c183f12acb8d9ecc2
asemjk <asemalsaiyadi@gmail.com>
2025-08-24 16:44:38 +0300

    ..

--- day3.py
+++ day3.py
@@ -21,3 +21,32 @@ print("""
                              `;;;;'
 
 """)
+direction = input("Please Enter direction,(Left or Right ?)")
+if(direction.lower() == "left"):
+    print("Game Over")
+    exit()
+elif(direction.lower() == "right"):
+    swimwait = input("Do you want to swim or wait?")
+    if(swimwait.lower() == "swim"):
+        print("Game Over")
+        exit()
+    elif(swimwait.lower() == "wait"):
+        doorcolor = input("Which color door do you want to enter?")
+        if(doorcolor.lower() == "red"):
+            print("Game Over")
+            exit()
+        elif(doorcolor.lower() == "blue"):
+            print("Game Over")
+            exit()
+        elif(doorcolor.lower() == "yellow"):
+            print("You Win")
+            exit()
+        else:
+            print("You Entered Wrong Color")
+            exit()
+    else:
+        print("You Entered Wrong Option")
+        exit()
+else:
+    print("You Entered Wrong Direction")
+    exit()
